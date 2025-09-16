b7bd3031c3220f5d7ddef76adadc9af23183322c
Jalil Teymoori <engineer.teymoori@gmail.com>
2025-09-17 00:41:04 +0330

    add constants and colors

---
+++ lib/core/constants/constants.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class AppConstants {
+  static const String hotelData = "assets/data/hotels.json";
+  static const String baeImageUrl =
+      "https://dunijet.ir/content/projects/hotelino";
+}
+
+class AppColors {
+  static const Color primry = Color(0xFFB27258);
+
+  //light colors
+  static const Color darkText = Colors.white;
+  static const Color darkHint = Colors.grey;
+  static const Color darkInputFill = Color(0xFF303030);
+  static const Color darkBorder = Color(0xFF707070);
+  static const Color darkFocusedBorder = Colors.brown;
+
+}
